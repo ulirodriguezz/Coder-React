38fb44fd0739c0cf2b17fea67a766957aa85b8be
ulirodriguezz <ulirodrigueze@gmail.com>
2025-04-25 18:14:27 -0300

    Entrega Final -Funcionalidad de vaciar carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,8 +4,9 @@ import CarItem from './CartItem'
 import CartContext from './context/CartContext'
 import { useNavigate } from 'react-router'
 import Swal from 'sweetalert2'
+import { toast } from 'react-toastify'
 function CartDetail() {
-    const { cart } = useContext(CartContext);
+    const { cart,emptyCart } = useContext(CartContext);
     const navigate = useNavigate();
     const calculateTotalCartPrice = () => {
         let total = 0;
@@ -24,9 +25,38 @@ function CartDetail() {
                 icon: 'warning',
                 confirmButtonText: 'Entendido'
             })
-        }else{
+        } else {
             navigate('/checkout')
         }
+    }
+    const handleEmptyCart = () =>{
+        if (cart.length == 0) {
+            toast.error('No hay nada en el carrito', {
+                position: "top-center",
+                autoClose: 1500,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: false,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }else{
+            Swal.fire({
+                title: '¿Estás seguro?',
+                text: 'Se eliminaran todos los productos de tu carrito',
+                icon: 'warning',
+                confirmButtonText: 'Vaciar Carrito',
+                showCancelButton:true,
+                cancelButtonText:'Cancelar'
+            }).then(result =>{
+                if(result.isConfirmed){
+                    emptyCart();
+                }
+            })
+        }
+
+
     }
     return (
         <section className={styles.cartDetailSection}>
@@ -51,7 +81,12 @@ function CartDetail() {
             </div>
             <div className={styles.confirmCartDiv}>
                 <h1>Total: ${calculateTotalCartPrice()}</h1>
-                <button className={styles.confirmCartButton} onClick={handleConfirm}>Confirmar Carrito</button>
+                <div className={styles.cartActionButtons}>
+                    <button className={styles.confirmCartButton} onClick={handleConfirm}>Confirmar Carrito</button>
+                    <button className={styles.emptyCartButton} onClick={handleEmptyCart}>
+                        <img className={styles.emptyCartIcon} src="../../src/assets/delete-icon.png" alt="algo" />
+                    </button>
+                </div>
 
             </div>
         </section>
